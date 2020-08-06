76e10201c0cda63c096a6cb07208e9ae6aceca56
Geoff deRosenroll <geoffderosenroll@gmail.com>
2020-08-06 11:33:28 -0700

    Dune project and fizzbuzz.

---
+++ playground/lib/fizzbuzz.ml
@@ -0,0 +1,18 @@
+open Base
+
+let ruleset =
+  let create_rule (label, num) = fun i ->
+    if i % num = 0 then Some label else None in
+  List.map ~f:create_rule
+
+let fizzer rules i =
+  List.map ~f:(fun f -> f i) rules
+  |> List.fold_left ~init:None ~f:(Option.merge ~f:( ^ ))
+  |> function | Some label -> label
+              | None -> Int.to_string i
+
+let fizzbuzz rule_list a b =
+  let rules = ruleset rule_list in
+  List.range a b |> List.map ~f:(fizzer rules)
+
+let result = fizzbuzz [ ("Fizz", 3); ("Buzz", 5) ] 1 31
