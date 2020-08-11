4619aa3afe141089616b1eb1d027346a2d03bc95
Geoff deRosenroll <geoffderosenroll@gmail.com>
2020-08-11 13:23:07 -0700

    Add a couple more basics.

--- playground/lib/snake.ml
+++ playground/lib/snake.ml
@@ -6,28 +6,42 @@ type 'a t =
   | Tip
   | Snek of 'a * 'a t
 
+let empty = Tip
+
+let is_empty = function
+  | Tip -> true
+  | _   -> false
+
 let grow a ss = Snek (a, ss)
 
 let ( @$ ) a ss = Snek (a, ss)
 
+let hd = function
+  | Tip -> None
+  | Snek (a, _) -> Some a
+
+let tl = function
+  | Tip -> None
+  | Snek (_, a) -> Some a
+
 let rec of_list = function
   | [] -> Tip
-  | hd :: tl -> hd @$ of_list tl
+  | h :: t -> h @$ of_list t
 
 let rev ss =
   let rec loop old new_snek =
     match old with
     | Tip -> new_snek
-    | Snek (ele, rest) -> grow ele new_snek |> loop rest in
+    | Snek (head, tail) -> grow head new_snek |> loop tail in
   loop ss Tip
 
 let rec map ~f = function
   | Tip -> Tip
-  | Snek (ele, rest) -> f ele @$ map ~f rest
+  | Snek (head, tail) -> f head @$ map ~f tail
 
 let map_rev ~f ss =
   let rec loop a new_snek =
     match a with
     | Tip -> new_snek
-    | Snek (ele, rest) -> f ele @$ new_snek |> loop rest in
+    | Snek (head, tail) -> f head @$ new_snek |> loop tail in
   loop ss Tip
