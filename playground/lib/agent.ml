e599f79a6b68170f92037d80cd9b622e02348a78
Geoff deRosenroll <geoffderosenroll@gmail.com>
2020-10-01 00:43:31 -0700

    Add timeout to post_and_reply. Reduce repetition.

--- playground/lib/agent.ml
+++ playground/lib/agent.ml
@@ -7,6 +7,8 @@ type 'b inbox = ('b, Pipe.Reader.phantom) Pipe.t
 
 type 'b answer = State of 'b | Stop
 
+let ( >> ) f g x = g (f x)
+
 let ( >?= ) answer loop = answer >>= function
   | State s -> loop s
   | Stop    -> return ()
@@ -26,31 +28,30 @@ let post : 'a t -> 'a -> unit Deferred.t = Pipe.write
 
 let close : 'a t -> unit = Pipe.close
 
-(* TODO: Add timeout option to post_and_reply. Also, maybe abstract out some
- * of the common stuff to repeat less in the variations. *)
-let post_and_reply t closure =
+let result_of_timeout = function
+  | `Result a -> Result.return a
+  | `Timeout  -> Result.fail "Timeout."
+
+let post_and_reply ?(timeout=86400.) t closure =
+  let times_up = Time.Span.of_sec timeout |> with_timeout in
   let (reply, channel) = Pipe.create () in
   let msg = closure channel in
   post t msg >>= fun () ->
-  Pipe.read reply >>= function
-  | `Eof -> Deferred.Result.fail "Reply channel closed(`Eof)"
-  | `Ok r -> Deferred.ok r
+  Pipe.read reply |> times_up >>| result_of_timeout >>=? function
+  | `Eof -> Deferred.Result.fail "Reply channel unexpectedly closed (`Eof)"
+  | `Ok r -> Deferred.Result.return r
+
+let post_and_reply_sync t closure =
+  (fun () -> post_and_reply t closure)
+  |> Thread_safe.block_on_async
+  |> Result.map_error ~f:(Error.of_exn >> Error.to_string_hum)
+  |> Result.join
 
 let post_and_reply_exn t closure =
-  let (reply, channel) = Pipe.create () in
-  let msg = closure channel in
-  post t msg >>= fun () ->
-  Pipe.read reply >>= function
-  | `Eof -> failwith "Reply channel closed(`Eof)"
-  | `Ok r -> return r
+  post_and_reply t closure >>| Result.ok_or_failwith
 
 let post_and_reply_sync_exn t closure =
-  let (reply, channel) = Pipe.create () in
-  let msg = closure channel in
-  post t msg >>= fun () ->
-  (fun () -> Pipe.read reply) |> Thread_safe.block_on_async_exn |> function
-  | `Eof -> failwith "Reply channel closed(`Eof)"
-  | `Ok r -> return r
+  (fun () -> post_and_reply_exn t closure) |> Thread_safe.block_on_async_exn
 
 module Test = struct
   type msg = Add of int | Show | Stop | Fetch of int t
@@ -73,7 +74,7 @@ module Test = struct
     post mailbox (Add 1)  >>= fun () ->
     post mailbox (Add 1)  >>= fun () ->
     post_and_reply_exn mailbox (fun c -> Fetch c) >>= fun reply ->
-    return (printf "Fetched state = %i" reply) >>= fun () ->
+    return (printf "Fetched state = %i" reply)  >>= fun () ->
     post mailbox Stop |> don't_wait_for;
     never_returns (Scheduler.go ())
 end
