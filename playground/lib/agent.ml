e9949ee3345418afb0ad147e8838e746ab37cd02
Geoff deRosenroll <geoffderosenroll@gmail.com>
2020-10-01 22:55:44 -0700

    Minor cleanup. Close reply channel on timeout.

--- playground/lib/agent.ml
+++ playground/lib/agent.ml
@@ -3,12 +3,14 @@ open Async
 
 type 'a t      = 'a Pipe.Writer.t
 type 'a inbox  = 'a Pipe.Reader.t
-type 'b answer = State of 'b | Async of 'b Deferred.t | Stop
 
-let read        : 'a inbox -> [> `Eof | `Ok of 'a ] Deferred.t = Pipe.read
-let post        : 'a t -> 'a -> unit Deferred.t                = Pipe.write
-let close       : 'a t -> unit                                = Pipe.close
-let close_inbox : 'a inbox -> unit                            = Pipe.close_read
+type 'b answer    = State of 'b | Async of 'b Deferred.t | Stop
+type ('b, 'a) body = 'b -> 'a -> 'b answer
+
+let read inbox  = Pipe.read inbox
+let post        = Pipe.write
+let close       = Pipe.close
+let close_inbox = Pipe.close_read
 
 let ( >> ) f g x = g (f x)
 
@@ -18,7 +20,7 @@ let ( >?= ) answer loop =
   | Async d -> d >>= loop
   | Stop    -> return ()
 
-let create ?(size=100) ~init body : 'a t =
+let create ?(size=100) ~init body =
   let (inbox, mailslot) = Pipe.create () in
   let () = Pipe.set_size_budget inbox size in
   let rec msg_loop state =
@@ -35,13 +37,16 @@ let result_of_timeout = function
   | `Timeout  -> Result.fail "Timeout."
 
 let post_and_reply ?(timeout=86400.) t closure =
-  let times_up = Time.Span.of_sec timeout |> with_timeout in
+  let timer m = with_timeout (Time.Span.of_sec timeout) m >>| result_of_timeout in
   let (inbox, channel) = Pipe.create () in
   let msg = closure channel in
   post t msg >>= fun () ->
-  read inbox |> times_up >>| result_of_timeout >>=? function
+  read inbox |> timer >>| (fun r -> close_inbox inbox; r) >>=? function
   | `Eof  -> Deferred.Result.fail "Reply channel unexpectedly closed (`Eof)"
-  | `Ok r -> close_inbox inbox; Deferred.Result.return r
+  | `Ok r -> Deferred.Result.return r
+
+let post_and_reply_exn t closure =
+  post_and_reply t closure >>| Result.ok_or_failwith
 
 (* NOTE: Warning, shouldn't be using block_on_async within async code. *)
 let post_and_reply_sync t closure =
@@ -49,9 +54,6 @@ let post_and_reply_sync t closure =
   |> Thread_safe.block_on_async
   |> Result.map_error ~f:(Error.of_exn >> Error.to_string_hum)
   |> Result.join
-
-let post_and_reply_exn t closure =
-  post_and_reply t closure >>| Result.ok_or_failwith
 
 (* NOTE: Warning, shouldn't be using block_on_async within async code. *)
 let post_and_reply_sync_exn t closure =
