549a35d62f9125ebbc81a04fa8538895c6821b80
Geoff deRosenroll <geoffderosenroll@gmail.com>
2020-10-14 12:14:01 -0700

    Example of writing over same line in stdout.

--- playground/lib/utils.ml
+++ playground/lib/utils.ml
@@ -12,3 +12,10 @@ let time_thunk_n_times ~n f =
   let () = Fn.apply_n_times ~n f () in
   Unix.gettimeofday () -. t
   |> Printf.printf "Execution time of %i repeats: %f seconds\n" n
+
+let stdout_overwrite_example () =
+  for i = 1 to 100 do
+    printf "\r%i" i;
+    Out_channel.flush stdout;
+    Unix.sleep 1
+  done
