776d84800ed668e2918b6f1492e541b4a1e9d802
Geoff deRosenroll <geoffderosenroll@gmail.com>
2022-01-30 12:39:12 -0800

    Another timing function for quick pasting

--- playground/lib/utils.ml
+++ playground/lib/utils.ml
@@ -3,8 +3,7 @@ open Core
 let time_thunk f =
   let t = Unix.gettimeofday () in
   let res = f () in
-  Unix.gettimeofday () -. t
-  |> Printf.printf "Execution time: %f seconds\n";
+  Unix.gettimeofday () -. t |> Printf.printf "Execution time: %f seconds\n";
   res
 
 let time_thunk_n_times ~n f =
@@ -12,6 +11,16 @@ let time_thunk_n_times ~n f =
   let () = Fn.apply_n_times ~n f () in
   Unix.gettimeofday () -. t
   |> Printf.printf "Execution time of %i repeats: %f seconds\n" n
+
+let get_time n f =
+  Gc.compact ();
+  let t = Unix.gettimeofday () in
+  for _i = 1 to n do
+    ignore (f ())
+  done;
+  let t2 = Unix.gettimeofday () in
+  Gc.compact ();
+  t2 -. t
 
 (* NOTE: "%!" is equivalent ot calling `Out_channel.flush stdout`. It does
  * not take any additional arguments. *)
